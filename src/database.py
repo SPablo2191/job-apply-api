6a286020f63538f5537ebff6a91c838d36ef9844
Pablo Sandoval <pablosandoval2191@gmail.com>
2024-05-03 17:14:52 -0300

    feat: add mongo database configuration

---
+++ src/database.py
@@ -0,0 +1,29 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
+from dotenv import load_dotenv
+import os
+load_dotenv()
+CONNECTION_STRING = os.environ['MONGODB_URI']
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Start the database connection
+    await startup_db_client(app)
+    yield
+    # Close the database connection
+    await shutdown_db_client(app)
+
+# method for start the MongoDb Connection
+async def startup_db_client(app):
+    app.mongodb_client = AsyncIOMotorClient(
+        CONNECTION_STRING)
+    app.mongodb = app.mongodb_client.get_database("college")
+    print("MongoDB connected.")
+
+# method to close the database connection
+async def shutdown_db_client(app):
+    app.mongodb_client.close()
+    print("Database disconnected.")
+
+
--- src/main.py
+++ src/main.py
@@ -1,15 +1,16 @@
 from fastapi import FastAPI
 from dotenv import load_dotenv
+from .database import lifespan
 import os
 
 from .auth.router import router as auth_router
 
 load_dotenv()
-app = FastAPI()
+app = FastAPI(lifespan=lifespan)
 app.title = "Job Application API"
 app.version = os.getenv('VERSION','0.0.0')
+
+
+# routes
 app.include_router(auth_router)
 
-@app.get("/")
-def hello_world():
-    return {"hello":"IT'S ME"}
