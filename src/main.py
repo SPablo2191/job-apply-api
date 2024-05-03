46246d81f7adf4443f73d64c2963cdad3fc6d710
Pablo Sandoval <pablosandoval2191@gmail.com>
2024-05-03 12:23:55 -0300

    feat: router from auth

--- src/main.py
+++ src/main.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
+import os
 
+from .auth.router import router as auth_router
+
+load_dotenv()
 app = FastAPI()
+app.title = "Job Application API"
+app.version = os.getenv('VERSION','0.0.0')
+app.include_router(auth_router)
 
 @app.get("/")
 def hello_world():
